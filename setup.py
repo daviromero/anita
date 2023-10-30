e932f013252b88a12c6a6bd29ad3bc17d6131ea5
Davi Romero de Vasconcelos <daviromero@ufc.br>
2023-10-30 15:09:41 -0300

    adiciona novas funcionalidades na execução

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='anita',
-    version='0.1.12',
+    version='0.1.13',
     license='MIT',
     author="Davi Romero de Vasconcelos",
     author_email='daviromero@ufc.br',
--- src/anita/__main__.py
+++ src/anita/__main__.py
@@ -10,16 +10,19 @@ def main():
     parser.add_argument("-t", type=str, help="Entre com o teorema a ser analisado.")
     parser.add_argument("-dl", type=int, default=0, help="Digite 1 para exibir o código LaTeX.")
     parser.add_argument("-dt", type=int, default=0, help="Digite 1 para exibir o teorema.")
+    parser.add_argument("-dc", type=int, default=0, help="Digite 1 para exibir o contra-exemplo do teorema.")
     args = parser.parse_args()
     input_theorem = None
     input_lang = "pt"
     input_display_latex = False
     input_display_theorem = False
+    input_display_countermodel = False
     if args.i is not None: fileName = args.i
     if args.t is not None: input_theorem = args.t
     if args.l is not None: input_lang = args.l
     if args.dl is not None: input_display_latex = (args.dl==1)
     if args.dt is not None: input_display_theorem = (args.dt==1)
+    if args.dc is not None: input_display_countermodel = (args.dc==1)
 
     if not os.path.isfile(fileName):
         return "Arquivo não encontradao"
@@ -27,9 +30,9 @@ def main():
 
     input_proof = f.read()
     if input_lang=="pt":
-        print(check_pt(input_proof,input_theorem=input_theorem,display_latex=input_display_latex, display_theorem=input_display_theorem))
+        print(check_pt(input_proof,input_theorem=input_theorem,display_latex=input_display_latex, display_theorem=input_display_theorem, display_countermodel=input_display_countermodel))
     elif input_lang=="en":
-        print(check_en(input_proof,input_theorem=input_theorem,display_latex=input_display_latex, display_theorem=input_display_theorem))
+        print(check_en(input_proof,input_theorem=input_theorem,display_latex=input_display_latex, display_theorem=input_display_theorem, display_countermodel=input_display_countermodel))
     else:
         print("Você deve escolher pt para Português (default) ou en para Inglês.")
     
